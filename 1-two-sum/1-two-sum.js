791a3be9ae9f7119bdbf47e9e2ed87ec8b47985f
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-05-24 11:04:52 -0400

    Time: 96 ms (64.98%), Space: 42.9 MB (40.42%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,20 +3,39 @@
  * @param {number} target
  * @return {number[]}
  */
-//when 2 numbers in an array need to be compared, running 2 loops is ideal 
+//--> O(N^2)
+// var twoSum = function(nums, target) {
+//     //for loop starting at 0
+//     //for loop at 1
+//     //if i + j = target return i & j 
+//     for (let i=0; i < nums.length; i++){
+//         for(let j=i+1; j < nums.length; j++){
+//             let sum = nums[i] + nums[j]
+//             if(sum === target){
+//                 return[i,j]
+//             }
+//         }
+//     }
+    
+    
+// }
+// [1,2,3,6,11], 5
+// target - nums[i] 
+//
 
-var twoSum = function(nums, target) {
-    //for loop starting at 0
-    //for loop at 1
-    //if i + j = target return i & j 
-    for (let i=0; i < nums.length; i++){
-        for(let j=i+1; j < nums.length; j++){
-            let sum = nums[i] + nums[j]
-            if(sum === target){
-                return[i,j]
-            }
+
+let twoSum = (nums, target) => {
+//
+    let memory ={}
+    for (let i = 0; i < nums.length; i++){
+        //-- if the position doesn't exist 
+        if(memory[nums[i]] === undefined){
+            //--subtract target by nums and equals index
+            memory[target-nums[i]] = i
+        }else{
+            return [i, memory[nums[i]]]
         }
     }
-    
+ 
     
 }
