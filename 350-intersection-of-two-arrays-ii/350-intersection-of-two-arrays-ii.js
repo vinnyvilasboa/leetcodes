f131ce3380923f75d4eb4e94ebf9979050c888a1
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-06-02 13:13:17 -0400

    Time: 116 ms (16.06%), Space: 44.1 MB (37.97%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -4,21 +4,27 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
+    //map 
     let obj = {}
+    //where the new array is gonna go
     let result = []
     
+    //for..of.. loop creates a loop over iterable objects
     for (let i of nums1){
+        //if there is nothing inside the object
         if(!obj[i]){
+            //let index = 1
             obj[i] = 1
-        
         } else {
+            //else loop through them 
             obj[i]++
         }
     }
-    console.log(obj)
 
     for (let i of nums2){
+        //if index is more than 0
         if (obj[i] > 0){
+            //push the index inside result vairable
             result.push(i)
             obj[i]--
         }
