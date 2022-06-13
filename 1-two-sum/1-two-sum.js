7db07b746483acb2d07a00c91a41cc63089050f1
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-06-13 08:48:14 -0400

    Time: 87 ms (74.69%), Space: 42.4 MB (75.32%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -18,15 +18,40 @@ loop 1 -> i = 0
 loop 2 -> j = i + 1 
 if loop1 + loop2 === target 
 return [loop1, loop2]
+
+TC: O(N^2) SC: 0(1)
+*/
+// let twoSum = (nums, target) => {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i + 1; j < nums.length;j++){
+//             if(nums[i] + nums[j] === target){
+//                 return [i,j]
+//             }
+//         }
+//     }
+// };
+
+
+/*HashMap
+- create hash map
+- Traverse the array
+- set var value to equals the index
+- set a new variable to equals target minus the value
+- set conditional if map[newVar] !== undefined since conditionals return bool false by default
+- ^ if the that number is inside the map return the number
+- else add that index "value" in the map
+TC: O(N) SC: 0(N)
 */
 
-
 let twoSum = (nums, target) => {
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length;j++){
-            if(nums[i] + nums[j] === target){
-                return [i,j]
-            }
+    let map = {};
+    for (let i = 0; i < nums.length; i++){
+        let value = nums[i]
+        let addedValue = target - value;
+        if(map[addedValue] !== undefined){
+            return [map[addedValue],i]
+        }else {
+            map[value] = i
         }
     }
-};
+}
