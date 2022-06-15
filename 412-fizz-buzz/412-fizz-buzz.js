a69a3249344db2ca3dda606f3b8458ed777ae04b
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-06-15 09:21:49 -0400

    Time: 136 ms (5.35%), Space: 44.1 MB (80.89%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let result = []
+    for (let i = 1; i <= n; i++){
+        if(i % 15 === 0){
+            result.push('FizzBuzz')
+        }else if(i % 5 === 0){
+            result.push('Buzz')
+        }else if(i % 3 === 0){
+            result.push('Fizz')
+        }else{
+            result.push(String(i)) 
+        }        
+    }
+    return result
+};
