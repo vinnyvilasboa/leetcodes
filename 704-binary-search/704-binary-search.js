9b24285ab813342e23f0bef79e6b7c9198fc38ae
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-07-18 14:08:53 -0400

    Time: 98 ms (45.37%), Space: 44.9 MB (78.68%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -3,30 +3,19 @@
  * @param {number} target
  * @return {number}
  */
--->my pseudo solution 1st attempt
-//2 params in function in var search
-//array of nums in ascending order
-//target number
-//write a function that searches the target num in the array nums. If target exists then return the "index" == loop
-//otherwise == else
 
-//loop through array, if nums index === target then return index[i] else return -1
-//when done optimize it to o(log n)
 
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-    while (left <= right) {
-        const middle = Math.floor(((left + right) / 2));
-        const current = nums[middle]
-        
-        //if middle of the nums array is more than the target parameter
-        if (current > target){
-            //let right variable update to be middle - 1
-            right = middle - 1
-        } else if (current < target) {
-            //if current is less the target then update left ?
-            left = middle + 1
+    let start = 0;
+    let end = nums.length - 1;
+    while (start <= end) {
+        const middle = Math.floor(((start + end) / 2));
+        const guess = nums[middle]
+    
+        if (guess > target){
+            end = middle - 1
+        } else if (guess < target) {
+            start = middle + 1
         }else {
             return middle
         }
