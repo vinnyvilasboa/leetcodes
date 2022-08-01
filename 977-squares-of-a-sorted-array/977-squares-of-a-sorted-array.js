ce08eb5683dab49bd2fa89825ea983b32cebf880
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-08-01 13:15:57 -0400

    Time: 185 ms (27.52%), Space: 48.4 MB (78.20%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -4,27 +4,24 @@
  */
 
 const sortedSquares = nums => {
-    //--> Creates New Array
-    const sorted = new Array(nums.length);
-    
+    //--> Creates New sorted Array
+    const sortedArray = new Array(nums.length);
     //--> 2 pointers
     let start = 0; //-- starts at 0 index
-    let end = sorted.length - 1; //-- starts at last index
-    
-    
+    let end = sortedArray.length - 1; //-- starts at last index
     for (let insert = end; insert >= 0; insert--) {
-    //--> the below variable are holding each iteration to the second power
-        const squaredStart = Math.pow(nums[start], 2);
-        const squaredEnd = Math.pow(nums[end], 2);
+        
+        const sqStart = Math.pow(nums[start], 2); //(base,exponent)
+        const sqEnd = Math.pow(nums[end], 2);
        
-        if (squaredStart > squaredEnd) {
-            sorted[insert] = squaredStart;
+        if (sqStart > sqEnd) { 
+            sortedArray[insert] = sqStart;
             start++;
         } else {
-            sorted[insert] = squaredEnd;
+            sortedArray[insert] = sqEnd;
             end--;
         }
     }
     
-    return sorted;
+    return sortedArray;
 };
