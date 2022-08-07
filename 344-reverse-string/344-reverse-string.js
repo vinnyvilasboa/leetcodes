b11df98312495dff49d618104e59f80b1eed97dd
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-08-07 14:23:11 -0400

    Time: 127 ms (65.58%), Space: 49.2 MB (74.90%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -4,13 +4,17 @@
  */
 
 var reverseString = function(s) {
-    let left = 0;
-    let right = s.length - 1;
-    while(left < right){
-        let hold = s[left];
-        s[left] = s[right];
-        s[right] = hold;
-        left++;
-        right--;
+//input is s array [h,e, l, l ,o]
+//2 points start and end
+    let start = 0
+    let end = s.length - 1;
+    while(start < end){
+        //make a placeholder to hold moving numbers -> start at s[0]
+        let firstIndex = s[start];
+        s[start] = s[end] //as it iterates it moves to end of array
+        s[end] = firstIndex //swaps place with s[0]
+        //iterate both points until loop conditional is met
+        start++;
+        end--;
     }
 };
