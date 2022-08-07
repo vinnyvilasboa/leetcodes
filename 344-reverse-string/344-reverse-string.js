958d1b263f9d93300169ad368a1d85890d8f673e
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-08-07 14:11:29 -0400

    Time: 149 ms (40.74%), Space: 49.7 MB (32.41%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,19 +3,6 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 
-/*BRUTE FORCE
-- It works if i just return s.reverse()
-
-*/
-// var reverseString = function(s) {
-//     return s.reverse()
-// };
-
-/*
-- 2 points that will eventually meet in the middle through iteration
-- left and right
-- while left is bigger than right -> once it's equal it'll stop iteration
-*/
 var reverseString = function(s) {
     let left = 0;
     let right = s.length - 1;
