ef009b1c9361b36a9059dda304266f668311a84a
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-08-09 07:45:39 -0400

    Time: 131 ms (25.74%), Space: 48.6 MB (47.68%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -3,23 +3,15 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-
-    let strArray = s.split(" ");
-    let result = [];
-
-    for (word of strArray) {
-        let l = 0;
-		let w = word.split('')
-        let r = w.length - 1;
-		while (l < r) {
-            let temp = w[l];
-            w[l] = w[r];
-            w[r] = temp;
-            l++;
-            r--;
+    let res = '';
+    let word = '';
+    for (let c of s) {
+        if (c === ' ') {
+            res += word + c;
+            word = '';
+        } else {
+            word = c + word;
         }
-        result.push(w.join(''));
     }
-
-    return result.join(" ");
+    return res + word;
 };
