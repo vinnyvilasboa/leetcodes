73836d3c50ca1b20774da10ef0fed51b7050f382
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2022-08-09 07:54:35 -0400

    Time: 109 ms (56.18%), Space: 48.5 MB (47.68%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -3,15 +3,33 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let res = '';
-    let word = '';
-    for (let c of s) {
-        if (c === ' ') {
-            res += word + c;
-            word = '';
-        } else {
-            word = c + word;
-        }
-    }
-    return res + word;
+	// 1. Split the string into an array of words
+    s = s.split(' ')
+	
+	// 2. Reverse the characters in each individual word
+    for (let word = 0; word < s.length; word++) {
+        s[word] = reverse(s[word])
+    } 
+	
+	// 3. Return the recombined string
+    return s.join(' ')
 };
+function reverse(word) {
+	// 1. Split the word into an array of characters
+    word = word.split('')
+	
+	// 2. Initialize our two pointers, left and right
+    for (let left = 0, right = word.length - 1; left < right; left++, right--) {
+		// 3. Swap the left and right characters with the help of a temp variable
+        const temp = word[left]
+        word[left] = word[right]
+        word[right] = temp
+		
+		// 4. Increment left and decrement right to move towards the middle of the array
+		// 5. Repeat the loop while left < right
+		// (The above steps are implemented in the definition of the for loop)
+    }
+
+	// 6. Join the array and return it as a string
+    return word.join('')
+}
